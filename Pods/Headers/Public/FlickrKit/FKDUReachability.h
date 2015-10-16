f59d5352d916354f8e1cb1b0dab0bb9de804d468
Tanvi Bhardwaj <tanvibhardwaj810@hotmail.com>
2015-10-16 12:03:00 +0530

    committing code

--- Pods/Headers/Public/FlickrKit/FKDUReachability.h
+++ Pods/Headers/Public/FlickrKit/FKDUReachability.h
@@ -1,1 +1,18 @@
-../../../FlickrKit/Classes/Network/FKDUReachability.h
+//
+//  FKDUReachability.h
+//  FlickrKit
+//
+//  Created by David Casserly on 30/05/2013.
+//  Copyright (c) 2013 DevedUp Ltd. All rights reserved. http://www.devedup.com
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FKDUReachability : NSObject
+
++ (BOOL) isConnected;
++ (BOOL) isOffline; // just the inverse of isConnected
+
++ (NSError *) buildOfflineErrorMessage;
+
+@end
